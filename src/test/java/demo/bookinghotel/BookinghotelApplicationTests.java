82d654d1f7e9c3260bd08b119815d0f0f2061cfc
minhhieu131103 <minhhieu131103@gmail.com>
2024-05-09 10:44:27 +0700

    message

---
+++ src/test/java/demo/bookinghotel/BookinghotelApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.bookinghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinghotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
